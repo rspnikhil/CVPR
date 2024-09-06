015d905afc1da9859a8da716dd419f9627a1c0fa
rspnikhil <136296207+rspnikhil@users.noreply.github.com>
2024-09-06 12:50:33 +0530

    config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-training_dir = r"/home/ravi/rsp/sig/benchmark/train"
-testing_dir = r"/home/ravi/rsp/sig/benchmark/test"
-training_csv = r"/home/ravi/rsp/sig/benchmark/train.csv"
-testing_csv =  r"/home/ravi/rsp/sig/benchmark/test.csv"
+training_dir = r"C:/Users/hp\Documents/GitHub/benchmark/train"
+testing_dir = r"C:/Users/hp\Documents/GitHub/benchmark/test"
+training_csv = r"C:/Users/hp\Documents/GitHub/benchmark/train.csv"
+testing_csv =  r"C:/Users/hp\Documents/GitHub/benchmark/test.csv"
 
 batch_size = 32
 epochs = 20
