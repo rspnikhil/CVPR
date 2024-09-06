9f00c1600bb343b1494598af1539056a3b2c8c35
rspnikhil <136296207+rspnikhil@users.noreply.github.com>
2024-09-06 12:41:02 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+training_dir = r"/home/ravi/rsp/sig/benchmark/train"
+testing_dir = r"/home/ravi/rsp/sig/benchmark/test"
+training_csv = r"/home/ravi/rsp/sig/benchmark/train.csv"
+testing_csv =  r"/home/ravi/rsp/sig/benchmark/test.csv"
+
+batch_size = 32
+epochs = 20
